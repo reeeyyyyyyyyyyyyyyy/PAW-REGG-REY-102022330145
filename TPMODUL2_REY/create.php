911cfd99607780b0a841ca057234c621ad67aa92
reeeyyyyyyyyyyyyyyy <muhammadryyyyy29@gmail.com>
2025-03-16 16:52:21 +0700

    tp2

---
+++ TPMODUL2_REY/create.php
@@ -0,0 +1,36 @@
+<?php
+include 'connect.php';
+
+// ==================1==================
+// If statement untuk mengecek POST request dari form
+// Lalu definisikan variabel-variabel untuk menyimpan data yang dikirim dari POST
+if (isset($_POST['create'])) {
+    $judulBuku = $_POST['judul'];
+    $penulisBuku = $_POST['penulis'];
+    $tahunTerbit = $_POST['tahun_terbit'];
+    
+    
+    // ==================2==================
+    // Definisikan $query untuk melakukan koneksi ke database\
+    $query = "INSERT INTO tb_buku (judul, penulis, tahun_terbit) VALUES ('$judulBuku', '$penulisBuku', '$tahunTerbit')";
+    mysqli_query($conn, $query);    
+
+
+
+    // ==================3==================
+    // Eksekusi query
+
+    if (mysqli_affected_rows($conn) > 0) {
+        header("location: katalog_buku.php");
+    } else {
+        echo "
+        <script>
+            alert('Data gagal ditambahkan');
+            document.location.href = 'katalog_buku.php';
+        </script>
+        ";
+        exit;
+    }
+}
+?>
+
